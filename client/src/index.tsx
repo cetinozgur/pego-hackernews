b3fd8ee71f367fc14a7d69ab6105914d4748cf82
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-11-21 01:04:53 +0300

    fix: auth0provider history, no need for redirect

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom/client";
 import { App } from "@/App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Auth0ProviderWithHistory } from "./Auth0ProviderWithHistory";
+import { Auth0Provider } from "@auth0/auth0-react";
+import {
+  REACT_APP_AUTH0_DOMAIN,
+  REACT_APP_AUTH0_CLIENT_ID,
+  REACT_APP_AUTH0_CALLBACK_URL,
+} from "./constants";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <Auth0ProviderWithHistory>
+        <Auth0Provider
+          domain={REACT_APP_AUTH0_DOMAIN}
+          clientId={REACT_APP_AUTH0_CLIENT_ID}
+          redirectUri={REACT_APP_AUTH0_CALLBACK_URL}
+        >
           <App />
-        </Auth0ProviderWithHistory>
+        </Auth0Provider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
