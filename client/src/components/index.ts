5977f2266c0a88ba5021493d5efd32b196755d5d
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-11-21 14:16:09 +0300

    refactor: add indexing for components & rearrange imports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,6 @@
+export * from "./navbar";
 export * from "./Layout";
+export * from "./LoginButton";
+export * from "./PageLoading";
+export * from "./ProtectedRoute";
+export * from "./SignupButton";
