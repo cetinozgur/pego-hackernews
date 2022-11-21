eaee16ed67802051bad61e1e26ba03ca3552ad3c
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-11-21 15:52:02 +0300

    add helmet middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ import { PORT } from "@/config/port.config";
 import { userRouter } from "@/routes/user.routes";
 import { checkJwt } from "@/middleware/auth0.middleware";
 import { CLIENT_ORIGIN_URL } from "./config/auth.config";
+import helmet from "helmet";
 
 AppDataSource.initialize()
   // eslint-disable-next-line @typescript-eslint/require-await
@@ -20,13 +21,30 @@ AppDataSource.initialize()
     const app = express(); // initialize express server
 
     // Middlewares
+    app.use(morgan("dev"));
     app.use(bodyParser.json());
     app.use(
       cors({
         origin: CLIENT_ORIGIN_URL,
       })
     );
-    app.use(morgan("dev"));
+    app.use(
+      helmet({
+        hsts: {
+          maxAge: 31536000,
+        },
+        contentSecurityPolicy: {
+          useDefaults: false,
+          directives: {
+            "default-src": ["'none'"],
+            "frame-ancestors": ["'none'"],
+          },
+        },
+        frameguard: {
+          action: "deny",
+        },
+      })
+    );
 
     // Routes
     app.use("/api/users", checkJwt, userRouter);
