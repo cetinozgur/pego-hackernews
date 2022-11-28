39fb619dcdde0a1dad0208888ae11c4345b7adc6
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-11-28 14:39:32 +0300

    remove express backend, proceed with apollo-server

--- server/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-import { UserController } from "@/controller/user.controller";
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", UserController.getAll);
-userRouter.post("/", UserController.create);
-userRouter.get("/:id", UserController.findById);
